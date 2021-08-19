ad048119db97208adb508f973ce9c147bcc4586d
Kurbanov Murad <Murad.KURBANOV@raiffeisen.ru>
2021-08-19 10:16:24 +0300

    Add move right and left

--- gun.py
+++ gun.py
@@ -1,10 +1,9 @@
 import pygame
 
 
-class Gun():
+class Gun:
 
     def __init__(self, screen):
-        """Инициализация пушки"""
 
         self.screen = screen
         self.image = pygame.image.load('images/pixel_gun.png')
@@ -12,6 +11,15 @@ class Gun():
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.m_right = False
+        self.m_left = False
 
     def output(self):
         self.screen.blit(self.image, self.rect)
+
+    def update_gun(self):
+        if self.m_right and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += 1
+
+        if self.m_left and self.rect.left > self.screen_rect.left:
+            self.rect.centerx -= 1
--- space_game.py
+++ space_game.py
@@ -1,25 +1,22 @@
 import pygame
-import sys
+import controller
 from gun import Gun
 
 
 def run():
 
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption('space_protections')
+    screen = pygame.display.set_mode((700, 800))
+    pygame.display.set_caption('py_game')
     bg_color = (0, 0, 0)
     gun = Gun(screen)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
+        controller.events(gun)
+        gun.update_gun()
         screen.fill(bg_color)
         gun.output()
         pygame.display.flip()
-
 
 
 run()
